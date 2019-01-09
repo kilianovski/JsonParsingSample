38d40ea24fe0aef5e85d7bf72aea36f16eebf22c
Mykhailo Kilianovskyi <mykhailo.kilianovskyi@sigma.software>
2019-01-09 12:53:59 +0200

    Initial commit

---
+++ WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+   public class MyModel
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // POST api/values
+        [HttpPost]
+        public ActionResult<string> Post([FromBody] MyModel model)
+        {
+            return $"Welcome, {model.Name}! I know, that you are {model.Age} y.o.";
+        }
+    }
+}
